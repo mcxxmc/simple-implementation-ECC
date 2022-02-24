2304fb1bd43a6e13d1585cdab402a697b2336054
Mingcheng Xu <44307188+mcxxmc@users.noreply.github.com>
2022-02-24 17:47:01 +0800

    coding improvement

--- test/ecdh_test.go
+++ test/ecdh_test.go
@@ -11,67 +11,28 @@ import (
 const loop = 100
 
 func TestECDH1(t *testing.T) {
-	ep := ecc.SampleElliptic()
-	ep.SetGeneratorPoint(15, 13)
 	rand.Seed(time.Now().Unix())
-	alice := ecc.NewInstanceECDH(ep)
-	bob := ecc.NewInstanceECDH(ep)
-
-	//ecc.SetDebug(true)
-
-	for i := 0; i < loop; i ++ {
-		alice.RandomlyPicksPrivateKey()
-		bob.RandomlyPicksPrivateKey()
-		publicKeyFromAlice := alice.PublicKey()
-		publicKeyFromBob := bob.PublicKey()
-		msg := "this is a message."
-		encrypted, err := alice.Encrypt(msg, publicKeyFromBob)
-
-		info := func() {
-			fmt.Println("loop number: ", i)
-			fmt.Println("alice private key: ", alice.PrivateKey)
-			fmt.Println("bob private key: ", bob.PrivateKey)
-			fmt.Println("public key from alice: ", publicKeyFromAlice)
-			fmt.Println("public key from bob: ", publicKeyFromBob)
-			if encrypted != nil {
-				fmt.Println("ciphertext: ", encrypted.Ciphertext)
-				fmt.Println("nonce: ", encrypted.Nonce)
-				fmt.Println("ciphertext public key: ", encrypted.CiphertextPubKey)
-			}
-		}
-
-		if err != nil {
-			info()
-			t.Error(err)
-			return
-		}
-		decrypted, err := bob.Decrypt(encrypted)
-		if err != nil {
-			info()
-			t.Error(err)
-			return
-		}
-		if decrypted != msg {
-			info()
-			fmt.Println(decrypted)
-			t.Error("wrong decryption result")
-			return
-		}
-	}
-
-	//ecc.SetDebug(false)
+	alice, bob := prepareECDH(0, 7, 17, 15, 13)
+	testECDHByLoop(alice, bob, loop, t)
 }
 
 func TestECDH2(t *testing.T) {
-	ep := ecc.NewElliptic(1, 1, 23)
-	ep.SetGeneratorPoint(3, 10)
 	rand.Seed(time.Now().Unix())
-	alice := ecc.NewInstanceECDH(ep)
-	bob := ecc.NewInstanceECDH(ep)
+	alice, bob := prepareECDH(1, 1, 23, 3, 10)
+	testECDHByLoop(alice, bob, loop, t)
+}
 
+func prepareECDH(a, b, p ,x, y int) (alice, bob *ecc.InstanceECDH) {
+	ep := ecc.NewElliptic(a, b, p)
+	ep.SetGeneratorPoint(x, y)
+	alice = ecc.NewInstanceECDH(ep)
+	bob = ecc.NewInstanceECDH(ep)
+	return
+}
+
+func testECDHByLoop(alice, bob *ecc.InstanceECDH, loopNum int, t *testing.T) {
 	//ecc.SetDebug(true)
-
-	for i := 0; i < loop; i ++ {
+	for i := 0; i < loopNum; i ++ {
 		alice.RandomlyPicksPrivateKey()
 		bob.RandomlyPicksPrivateKey()
 		publicKeyFromAlice := alice.PublicKey()
@@ -110,6 +71,5 @@ func TestECDH2(t *testing.T) {
 			return
 		}
 	}
-
 	//ecc.SetDebug(false)
 }
